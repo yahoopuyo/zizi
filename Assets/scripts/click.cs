8d4bad4399870ea7acf1180b9db6a18acd034a72
admin <naoaki65k@gmail.com>
2019-03-02 21:51:56 +0900

    マウスに反応するようにして、クリック回数を一回で反応するようにした。drawnsの位置とカードの表裏も少し変えた

--- Assets/scripts/click.cs
+++ Assets/scripts/click.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Click : MonoBehaviour , IPointerClickHandler
+public class Click : MonoBehaviour , IPointerClickHandler, IPointerEnterHandler, IPointerExitH​​andler
 {
     private GameObject hand;
     private GameObject gameManager;
@@ -12,23 +12,50 @@ public class Click : MonoBehaviour , IPointerClickHandler
     private Distribute distribute;
     CardModel cardModel;
     int cardIndex;
+    int turnPlayer;
+    int drawnPlayer;
+
+    void get()
+    {
+        gameManager = GameObject.Find("GameManager");
+        turnManager = gameManager.GetComponent<TurnManager>();
+        turnPlayer = turnManager.turnPlayer;
+        drawnPlayer = turnManager.drawnPlayer;
+        hand = GameObject.Find("Hand"); //Handのクラスを取得
+        hands = hand.GetComponent<Hands>();
+        cardModel = GetComponent<CardModel>();
+        cardIndex = cardModel.cardIndex; //カードモデルからcardIndexを取得
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        get();
+
+        if (drawnPlayer == hands.Cardownerreturn(cardIndex))
+        {
+            var v = new Vector2(0, 0.4f);
+            transform.Translate(v);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        get();
+
+        if (drawnPlayer == hands.Cardownerreturn(cardIndex))
+        {
+            var v = new Vector2(0,-0.4f);
+            transform.Translate(v);
+        }
+    }
+
     void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
     {
-        if (eventData.clickCount > 1) //クリック回数>1の時
+        if (eventData.clickCount > 0) //クリック回数>0の時  一応残すけどこのままいくならif文とっても良いはず
         {
-            gameManager = GameObject.Find("GameManager");
-            turnManager = gameManager.GetComponent<TurnManager>();
-            int turnPlayer = turnManager.turnPlayer;
-            int drawnPlayer = turnManager.drawnPlayer;
-            hand = GameObject.Find("Hand"); //Handのクラスを取得
-            hands = hand.GetComponent<Hands>();
+            get();
 
-    //        Debug.Log(eventData.clickCount); //クリックされた回数を表示
-
-            cardModel = GetComponent<CardModel>();
-            cardIndex = cardModel.cardIndex; //カードモデルからcardIndexを取得
-
-            if(drawnPlayer == hands.Cardownerreturn(cardIndex))
+            if (drawnPlayer == hands.Cardownerreturn(cardIndex))
             {
                 transform.Rotate(0, 0, 90);  //カードを90度回転
                 hands.hands[drawnPlayer].Remove(cardIndex); //引かれる人の手札配列からカードを削除
